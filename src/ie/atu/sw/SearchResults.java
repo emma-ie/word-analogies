31800b0b05d7f2bd85590f36456bd5880ad93fbd
emma <emmakelly5500@gmail.com>
2026-05-12 11:49:01 +0100

    add SearchResults class, implement constructor and getters

---
+++ src/ie/atu/sw/SearchResults.java
@@ -0,0 +1,22 @@
+package ie.atu.sw;
+
+public class SearchResults {
+
+	private String word;
+	private double score;
+	
+	// Constructor to set variables 
+	public SearchResults(String word, double score) {
+		this.word = word;
+		this.score = score;
+	}
+
+	public String getWord() {
+		return word;
+	}
+	
+	public double getScore() {
+		return score;
+	}
+		
+}
